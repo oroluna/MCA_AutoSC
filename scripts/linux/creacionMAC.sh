0531edf9640f7caa571ce604bb632553cc8e1554
dianagac <101782050+dianagac@users.noreply.github.com>
2022-05-10 16:38:26 -0500

    Add file creacionMAC

---
+++ scripts/linux/creacionMAC.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/autosc/Documents/MCA_AutoSC-main/Crear_MAC
+
+echo "Eliminando archivos .mac anteriores..."
+rm -r /home/autosc/Documents/MCA_AutoSC-main/data/1_MAC/VaryE/mu-/*.mac
+rm -r /home/autosc/Documents/MCA_AutoSC-main/data/1_MAC/VaryE/e-/*.mac
+rm -r /home/autosc/Documents/MCA_AutoSC-main/data/1_MAC/VaryE/gamma/*.mac
+
+echo "Creando archivos .mac con particula mu-"
+python3 mac_files_config.py -i 12 --config_json config_mu.json -d ../data/1_MAC/VaryE/mu-
+
+echo "Creando archivos .mac con particula e-"
+python3 mac_files_config.py -i 12 --config_json config_e.json -d ../data/1_MAC/VaryE/e-
+
+echo "Creando archivos .mac con particula gamma"
+python3 mac_files_config.py -i 12 --config_json config_gamma.json -d ../data/1_MAC/VaryE/gamma
